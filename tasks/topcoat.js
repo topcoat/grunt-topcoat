dd1ed8fcaedc1cb97419d39162b6bfacd9f4e269
kristoferjoseph <kristoferjoseph@gmail.com>
2013-06-18 16:01:54 -0700

    Adding copyright header

--- tasks/topcoat.js
+++ tasks/topcoat.js
@@ -1,5 +1,18 @@
-/*
- * grunt-topcoat
+/**
+ *
+ * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  */
 
@@ -90,8 +103,7 @@ module.exports = function(grunt) {
                     'url': url,
                     'encoding': 'binary',
                     'headers': {'user-agent': 'topcoat.io'},
-                    'proxy': proxy
-                }, function(error, response, body) {
+                    'proxy': proxy }, function(error, response, body) {
                     if (!error) {
                         fs.writeFileSync(path, body, 'binary');
                     } else {
