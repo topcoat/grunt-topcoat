c5c953cdb3f1d5bf7de385a2b3a4d011996673ee
kristoferjoseph <kristoferjoseph@gmail.com>
2013-11-18 11:58:26 -0800

    Updating tests. Still failing due to something unseen in files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
 
         // Before generating any new files, remove any previously-created files.
         clean: {
-            tests: ['tmp']
+            tests: ['tmp/*']
         },
 
         // Configuration to be run (and then tested).
